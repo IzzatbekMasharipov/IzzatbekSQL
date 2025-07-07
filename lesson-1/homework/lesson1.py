a81529bf9b7d0080d4c983014a9de79230869115
IzzatbekMasharipov <masharipovizzatbek562@gmail.com>
2025-07-07 22:04:50 +0300

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,26 @@
+task 1
+# kvadrat perimetri va yuzini topish
+a=int(input("Kvadratning a tamonini kiriting: "))
+peremetr=4*a
+yuzasi=a*a
+print("kvadratning peremetri =",peremetr,"\nkvadratning yuzasi =",yuzasi)
+task 2
+# aylana uzunligi  topish formulasi
+d=float(input("aylana diametrini kiriting: "))
+c=3.14*d
+print("aylana uzunligi = ",c)
+task 3
+# arifmetik o'rtacha topish
+a=float(input("birinchi sonni kiriting: "))
+b=float(input("ikkinchi sonni kiriting: "))
+c=(a+b)/2
+print("berilgan sonlarning arifmetik o'rtachasi = ",c)
+task 4
+#sonlarning yig'indisi,ko'paytmasi va har birining kvadratini topish
+a=float(input("a sonini kiriting: "))
+b=float(input("b sonini kiriting: "))
+c=a+b
+v=a*b
+n=pow(a,2)
+m=pow(b,2)
+print("a+b=",c,"\na*b=",v,"\na^2=n",n,"\nb^2=",m)
