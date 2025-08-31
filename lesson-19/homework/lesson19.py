ccb18547bdffadab03b77c223b0102b8b66f8a34
IzzatbekMasharipov <masharipovizzatbek562@gmail.com>
2025-08-31 16:23:24 +0500

    Create lesson19.py

---
+++ lesson-19/homework/lesson19.py
@@ -0,0 +1,109 @@
+homework 2
+task 1
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['creationdate']<'2014-01-01']
+
+task 2
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['score']>50]
+task 3
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['score'].between(50,100)]
+task 4
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['ans_name']=='Scott Boston']
+task 5
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['id'].isin([332289,2051744,2225995,5486226,5515021])]
+task 6
+df = pd.read_csv('stackoverflow_qa.csv')
+creation=df['creationdate'].between("2014-03-01","2014-10-01")
+ans_name=df['ans_name']=='unutbu'
+ckore_5=df['score']<5
+df[creation&ans_name&ckore_5]
+task7
+df = pd.read_csv('stackoverflow_qa.csv')
+df[(df['score'].between(5,10))|(df['viewcount']>10000)]
+task 8
+df = pd.read_csv('stackoverflow_qa.csv')
+df[df['ans_name']!='Scott Boston']
+homework 3
+task 1
+titanic_df = pd.read_csv('titanic.csv')
+new_titanic=titanic_df[(titanic_df['Age'].between(20,30))&(titanic_df['Pclass']==1)]
+new_titanic
+
+task 2
+titanic_df = pd.read_csv('titanic.csv')
+
+fire_100=titanic_df[titanic_df['Fare']>100]
+task 3
+titanic_df = pd.read_csv('titanic.csv')
+
+ew=titanic_df[(titanic_df['SibSp']==0)&(titanic_df['Parch']==0)&(titanic_df['Survived']==1)]
+task 4
+dc=titanic_df[(titanic_df['Embarked']=='C')&(titanic_df['Fare']>50)]
+task 5
+dq=titanic_df[(titanic_df['SibSp']==1)&(titanic_df['Parch']==1)]
+task 6
+df=titanic_df[(titanic_df['Age']<=15)&(titanic_df['Survived']!=1)]
+task 7
+dw=titanic_df[(titanic_df['Cabin'].notnull())&(titanic_df['Fare']>200)]
+task 8
+dr=titanic_df[titanic_df['PassengerId']%2!=0]
+task 9
+unique_tickets_df=titanic_df[titanic_df['Ticket'].duplicated(keep=False)==False]
+task 10
+new_datafarm=titanic_df[(titanic_df['Pclass']==1)&(titanic_df['Name'].str.contains('Miss',case=False))&(titanic_df['Sex']=='female')]
+
+task 3
+import sqlite3
+with sqlite3.connect("population.db") as conn:
+    db=pd.read_sql_query("select * from population",conn)
+db   
+data=pd.read_excel("population_salary_analysis.xlsx") 
+db
+def Percentage(salary):
+    if salary<=200000:
+        return 'till $200,000'
+    elif salary>200000 and salary<400000:
+        return '$200,001 - $400,000'
+    elif salary>400000 and salary<600000:
+        return '$400,001 - $600,000'
+    elif salary>600000 and salary<800000:
+        return '$600,001 - $800,000'
+    elif salary>800000 and salary<1000000:
+        return '$800,001 - $1,000,000'
+    elif salary>1000000 and salary<1200000:
+        return '$1,000,001 - $1,200,000'
+    elif salary>12000000 and salary<1400000:
+        return '$1,200,001 - $1,400,000'
+    elif salary>14000000 and salary<1600000:
+        return '$1,400,001 - $1,600,000'
+    elif salary>16000000 and salary<1800000:
+        return '$1,600,001 - $1,800,000'
+    elif salary>1800000:
+        return '$1,800,001 and over'
+    else:
+        return 'unknown'
+db['salary'].apply(Percentage).value_counts(normalize=True)*100
+db['Salary Band']=db['salary'].apply(Percentage)
+db
+
+Average_salary=db.groupby('Salary Band')['salary'].mean()
+Average_salary
+
+
+Median_Salary =db.groupby('Salary Band')['salary'].median()
+Median_Salary
+
+Number_of_populition=db.groupby('Salary Band')['salary'].count()
+Number_of_populition
+
+
+state_summary=db.groupby(['state','Salary Band'])
+
+state_summary=db.groupby(['state','Salary Band']).agg(salary_count=('salary','count'),salary_mean=('salary','mean'),salary_median=('salary','median'))
+state_summary
+
+
